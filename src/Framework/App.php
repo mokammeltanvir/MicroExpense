d571a1681588d0888bcf75f35cf49a87c22621fe
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-03-10 22:19:22 +0600

    Initial App Setup

---
+++ src/Framework/App.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+class App
+{
+    public function run(): void
+    {
+        echo 'Hello, world!';
+        echo '<br>';
+        echo 'App is running...';
+    }
+}
