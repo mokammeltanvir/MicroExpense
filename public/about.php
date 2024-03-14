fc0658fda8ed35397f8ee099a0c274de1ca35371
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-03-14 14:32:07 +0600

    Complete Project

--- public/about.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// About page 
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,11 +2,20 @@
 
 declare(strict_types=1);
 
-include __DIR__ . "/../Framework/App.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Config\Paths;
+use Dotenv\Dotenv;
 
+use function App\Config\{registerRoutes, registerMiddleware};
 
-$app = new App();
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
+
+$app = new App(Paths::SOURCE . "App/container-definitions.php");
+
+registerRoutes($app);
+registerMiddleware($app);
 
 return $app;
